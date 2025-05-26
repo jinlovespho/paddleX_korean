968c60af424032f8050c7001f7b60a6aa2a62e42
jinlovespho <msjchr922@gmail.com>
2025-05-26 11:35:03 +0000

    250526

--- scripts/test/run_test.sh
+++ scripts/test/run_test.sh
@@ -1,7 +1,17 @@
 python test.py \
     --pipeline OCR \
     --korean_font_path fonts/NanumGothic.ttf \
-    --test_imgs_path /media/dataset2/jinlovespho/plantynet/data/filtered_train_test/test_images \
-    --test_anns_path /media/dataset2/jinlovespho/plantynet/data/filtered_train_test/test_anns \
-    --save_root_path ./test_results/trained_det_rec \
+    --test_imgs_path /SET/PATH/TO/TESTING/IMAGES \
+    --test_anns_path /SET/PATH/TO/TESTING/ANNOTATIONS \
+    --save_root_path /SET/PATH/TO/SAVING/RESULTS \
     --gpu 1 \
+
+
+# example script
+# python test.py \
+#     --pipeline OCR \
+#     --korean_font_path fonts/NanumGothic.ttf \
+#     --test_imgs_path /media/dataset2/jinlovespho/plantynet/data/filtered_train_test/test_images \
+#     --test_anns_path /media/dataset2/jinlovespho/plantynet/data/filtered_train_test/test_anns \
+#     --save_root_path ./test_results/trained_det_rec \
+#     --gpu 1 \
